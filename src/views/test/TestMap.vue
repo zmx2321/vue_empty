15ae56bd95bbce2576b7fe988e4f8d5c27c4c139
zmx2321 <zmx2321@163.com>
2021-09-16 15:03:17 +0800

    init

--- src/views/test/TestMap.vue
+++ src/views/test/TestMap.vue
@@ -14,11 +14,20 @@ export default {
             zoom: 7,
             events: {
                 init: o=> {
+                    // 供出地图的实例
                     window.amapview = o
+
+                    // 获取地图信息
+                    this.getMapInfo()
+
+                    // 获取geojson
+                    this.getGeoJson()
 
                     // o.setMapStyle('amap://styles/macaron');//自定义的高德地图的样式，我选的是马卡龙
                 }
-            }
+            },
+
+            isInit: true
         }
     },
 
@@ -26,16 +35,35 @@ export default {
     },
 
     methods: {
-        getGeoJson() {
+        // geojson api示例
+        getGeoJsonExample() {
             this.axios.get("https://a.amap.com/jsapi_demos/static/geojson/chongqing.json").then(res=> {
                 // console.log(res.data)
 
                 let geoJSONData = res.data;
 
-                let geojson = new AMap.GeoJSON({
+                let geojsonLayer = new AMap.GeoJSON({
+                    // 要加载的标准GeoJSON对象
                     geoJSON: geoJSONData,
-                    // 还可以自定义getMarker和getPolyline
-                    getPolygon: function(geojson, lnglats) {
+
+                    /* // 指定点要素的绘制方式，缺省时为Marker的默认样式。
+                    // geojson为当前要素对应的GeoJSON对象，lnglat为对应的点的位置
+                    getMarker(geojson, lnglats) {
+                        console.log("点")
+                    },
+
+                    // 指定线要素的绘制方式，缺省时为Polyline的默认样式。
+                    // geojson为当前要素对应的GeoJSON对象，lnglats为对应的线的路径
+                    getPolyline(geojson, lnglats) {
+                        console.log("线")
+                    }, */
+
+                    // 指定面要素的绘制方式，缺省时为Polygon的默认样式。
+                    // geojson为当前要素对应的GeoJSON对象，lnglats为对应的面的路径
+                    getPolygon(geojson, lnglats) {
+                        // console.log("面")
+                        // console.log(geojson)
+
                         // 计算面积
                         let area = AMap.GeometryUtil.ringArea(lnglats[0])
 
@@ -48,15 +76,197 @@ export default {
                     }
                 })
 
+                console.log(geojsonLayer)
                 this.$message.success("geojson加载成功")
 
-                geojson.setMap(window.amapview);
+                // 加载新的GeoJSON对象，转化为覆盖物，旧的覆盖物将移除
+                // geojsonLayer.importData(geoJSONData)
+
+                // 将当前对象包含的覆盖物转换为GeoJSON对象
+                // console.log("将当前对象包含的覆盖物转换为GeoJSON对象", geojsonLayer.toGeoJSON())
+
+                // 添加一个覆盖物，如需要在转成GeoJSON的时候将某些信息带给对应GeoJSON对象的properties属性中，
+                // 可以将信息添加到覆盖物的extData的_geoJsonProperties字段中
+                /* let marker = new AMap.Marker({
+                    position:[107.943579, 30.131735],
+                    extData:{_geoJsonProperties:{
+                        name:'marker1',
+                        icon:'xx.png',
+                        }
+                    }
+                });
+                geojsonLayer.addOverlay(marker); */
+
+                // 添加多个覆盖物，说明同addOverlay
+                // addOverlays(overlays: Array)
+
+                // 获取所有覆盖物
+                // console.log(geojsonLayer.getOverlays())
+
+                // 隐藏所有覆盖物
+                // geojsonLayer.hide()
+
+                // 显示所有覆盖物
+                // geojsonLayer.show()
+
+                // 遍历覆盖物
+                geojsonLayer.eachOverlay(iterator => {
+                    console.log(iterator)
+                })
+
+                // 必须是异步才能获取地图对象
+                console.log(window.amapview)
+
+                // 设置地图
+                geojsonLayer.setMap(window.amapview);
             }).catch({})
+        },
+
+        // 获取地图信息
+        getMapInfo() {
+            console.log("AMap", AMap)
+
+            // 必须是异步
+            setTimeout(() => {
+                console.log("amapview", window.amapview)
+
+                window.amapview.on('click', e=> {
+                    // console.log("地图点击事件", e)
+
+                    // 地图坐标
+                    this.getPosition(e)
+                })
+            }, 0);
+        },
+
+        // 地图坐标
+        getPosition(e) {
+            console.log('您在 ['+e.lnglat.getLng()+','+e.lnglat.getLat()+'] 的位置点击了地图');
+        },
+
+        // 初始化geojson
+        initGeojsonLayer(data) {
+            let that = this;
+
+            return new AMap.GeoJSON({
+                // 要加载的标准GeoJSON对象
+                geoJSON: data,
+
+                // 指定面要素的绘制方式，缺省时为Polygon的默认样式。
+                // geojson为当前要素对应的GeoJSON对象，lnglats为对应的面的路径
+                getPolygon(geojson, lnglats) {
+                    // console.log(geojson)
+
+                    // 计算面积
+                    // let area = AMap.GeometryUtil.ringArea(lnglats[0])
+
+                    return new AMap.Polygon({
+                        path: lnglats,
+                        // fillOpacity: 1 - Math.sqrt(area / 8000000000),// 面积越大透明度越高
+                        // fillOpacity: 0.5,
+                        strokeColor: 'white',
+                        fillColor: 'blue'
+                    });
+                }
+            })
+        },
+
+        // 获取geojson
+        getGeoJson() {
+            this.axios.get("https://a.amap.com/jsapi_demos/static/geojson/chongqing.json").then(res=> {
+                // console.log(res.data)
+
+                // 获取geojson数据
+                let geoJSONData = res.data;
+
+                // 初始化geojson，获取geojson地图对象
+                let geojsonLayer = this.initGeojsonLayer(geoJSONData)
+
+                // 隐藏所有覆盖物
+                // geojsonLayer.hide()
+
+                // 遍历geojson地图对象，绑定事件
+                geojsonLayer.eachOverlay(iterator => {
+                    // console.log(iterator)
+
+                    iterator.on('click', e=> {
+                        // console.log("地图点击事件", e)
+                        // console.log("geojson单个对象", iterator)
+
+                        // 获取geojson事件
+                        this.getGeoEvent(e, iterator)
+                    })
+                })
+
+                this.$message.success("geojson加载成功")
+
+                // console.log("amapview", window.amapview)
+                // console.log(geojsonLayer)
+
+                // 设置地图
+                geojsonLayer.setMap(window.amapview);
+            }).catch({})
+        },
+
+        // 获取geojson事件
+        getGeoEvent(e, geoitem) {
+            // console.log('点击geojson ['+e.lnglat.getLng()+','+e.lnglat.getLat()+']');
+            // console.log("geojson单个对象", geoitem)
+
+            let geojsonItem = geoitem.toGeoJSON()
+            // console.log("地图对象转geojson", geojsonItem)
+
+            console.log("区县名称", geojsonItem.properties.name)
+
+            // 设置颜色
+            let geojsonLayerItem = new AMap.GeoJSON({
+                // 要加载的标准GeoJSON对象
+                geoJSON: geojsonItem,
+
+                getPolygon(geojson, lnglats) {
+                    // console.log(geojson)
+
+                    return new AMap.Polygon({
+                        path: lnglats,
+                        // fillOpacity: 0.5,
+                        strokeColor: 'white',
+                        fillColor: 'red'
+                    });
+                }
+            })
+
+            geojsonLayerItem.setMap(window.amapview);
+
+            // 点击之后鼠标移除事件
+            geoitem.on('mouseout', ()=> {
+                // console.log("点击之后鼠标移除事件")
+
+                // console.log(geoitem.w.fillOpacity);
+
+                // this.getGeoJson();
+                let geojsonLayerItemInit = new AMap.GeoJSON({
+                    // 要加载的标准GeoJSON对象
+                    geoJSON: geojsonItem,
+
+                    getPolygon(geojson, lnglats) {
+                        // console.log(geojson)
+
+                        return new AMap.Polygon({
+                             path: lnglats,
+                            // fillOpacity: 0.5,
+                            strokeColor: 'white',
+                            fillColor: 'blue'
+                        });
+                    }
+                })
+                
+                geojsonLayerItemInit.setMap(window.amapview);
+            })
         }
     },
 
     created() {
-        this.getGeoJson()
+        
     },
 
     mounted() {
