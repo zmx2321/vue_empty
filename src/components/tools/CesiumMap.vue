dfc2265d86f804885aed22828a2bd94f489cf1a6
zmx2321 <zmx2321@163.com>
2021-09-30 11:05:36 +0800

    init

--- src/components/tools/CesiumMap.vue
+++ src/components/tools/CesiumMap.vue
@@ -10,14 +10,14 @@ export default {
         return {
             // 场景初始化
             resetViewer: {
-                sceneMode : Cesium.SceneMode.SCENE2D, // 初始场景模式 为二维
+                // sceneMode : Cesium.SceneMode.SCENE2D, // 初始场景模式 为二维
                 animation: false,  // 动画控制     
                 timeline: false,    // 时间线
                 fullscreenButton: false, // 全屏按钮
                 geocoder: true,  // 查找位置工具，查找到之后会将镜头对准找到的地址，默认使用bing地图
                 homeButton: false,  // 视角返回初始位置
                 sceneModePicker: false,  // 选择视角的模式，有三种：3D，2D，哥伦布视图（CV）
-                baseLayerPicker: false,  //  图层选择器，选择要显示的地图服务和地形服务
+                baseLayerPicker: true,  //  图层选择器，选择要显示的地图服务和地形服务
                 navigationHelpButton: false,  // 导航帮助按钮，显示默认的地图控制帮助
                 // scene3DOnly : true,//如果设置为true，则所有几何图形以3D模式绘制以节约GPU资源
             }
--- src/views/test/cesiumtest/TestCesiumMap1.vue
+++ src/views/test/cesiumtest/TestCesiumMap1.vue
@@ -41,8 +41,6 @@ export default {
 
 <style lang="less" scoped>
 .main_cont {
-    width: 100%;
-    height: 100%;
     padding: 0
 }
 </style>
