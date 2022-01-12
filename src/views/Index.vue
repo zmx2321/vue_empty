cc08485674456b6821f3e6bcee393b3c7047d018
zmx2321 <zmx2321@163.com>
2022-01-12 08:36:55 +0800

    init

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,17 +1,17 @@
 <template>
-    <el-container class="container f-pr">
-        <el-header class="header f-oh">
+    <section class="container">
+        <header class="header f-oh">
             <HeadNav></HeadNav>
-        </el-header>
-        <el-container>
-            <el-aside class="aside f-oh">
+        </header>
+        <section class="main_wrap">
+            <aside class="aside f-oh">
                 <LeftMenu></LeftMenu>
-            </el-aside>
-            <el-main class="main f-oh">
-                <router-view></router-view>
-            </el-main>
-        </el-container>
-    </el-container>
+            </aside>
+            <section class="main">
+                <router-view />
+            </section>
+        </section>
+    </section>
 </template>
 
 <script>
@@ -32,27 +32,36 @@ export default {
 </script>
 
 <style lang="less" scoped>
-    .container {
+.container {
+    width: 100%;
+    height: 100%;
+
+    .header, .aside{
+        background: @mainBg;
+    }
+
+    .header{
+        height: @headNavHeight !important;
+        padding: 0;
+        border-bottom: 1px solid #1f2d3d;
+    }
+
+    .main_wrap {
+        display: flex;
         width: 100%;
-        height: 100%;
-
-        .header, .aside{
-            background: #324057;
-        }
-
-        .header{
-            height: @headNavHeight !important;
-            padding: 0;
-            border-bottom: 1px solid #1f2d3d;
-        }
+        height: calc(100vh - @headNavHeight - 1px);
 
         .aside {
-            flex: 0 0 230px;
-            width: 230px;
+            width: @sideBarWidth;
+            height: 100%;
         }
 
         .main {
+            width: calc(100vw - @sideBarWidth - 40px);
+            flex: 1;
             padding: 0;
+            overflow: auto;
         }
     }
+}
 </style>
